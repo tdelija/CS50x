13914c5458eb6649c5b277e2fb8e97b253039ca2
Tony Delija <tdelija@gmail.com>
2021-02-20 23:52:31 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CS50x.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS50x.dir/main.c.o"
+  "CS50x"
+  "CS50x.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS50x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
